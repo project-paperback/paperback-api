3b462160e2ffd024dbc8479d8bef1625923f0084
Riccardo Foti <riccardofoti97@gmail.com>
2024-04-04 12:03:24 +0100

    - rewrite schema based on new json books

--- SeedingFiles/seedingDB.js
+++ SeedingFiles/seedingDB.js
@@ -1,39 +1,15 @@
 const mongoose = require("mongoose")
-const Book = require("../schema/BookSchema.js")
-const fs = require("fs").promises
-const dataPromise = fs.readFile("info_books.json", "utf-8")
-
+const Book = require("../database/schema/bookSchema.js")
+// const fs = require("fs").promises
+// const dataPromise = fs.readFile("info_books.json", "utf-8")
+const bookData = require("../info_books.json")
 
 async function saveBooks() {
   try {
     await mongoose.connect(
       "mongodb+srv://riccardofoti97:9DjR06YkoRabUZcS@bookshop.wtlyola.mongodb.net/development?retryWrites=true&w=majority&appName=BookShop"
     ); // Await connection completion
-    const pendingPromise = await dataPromise
-    const data = JSON.parse(pendingPromise)
-    const promises = data.map(async (book) => {
-      const existingBook = await Book.findOne({ title: book.title });
-      console.log(existingBook);
-      if (existingBook === null) {
-        const newBook = new Book({
-          // _id: new ObjectId(book._id),
-          title: book.title,
-          author: book.author,
-          year: book.year,
-          pages: book.pages,
-          genres: book.genres,
-          language: book.language,
-          isFiction: book.isFiction,
-          publisher: book.publisher,
-          bookPrice: book.bookPrice,
-          isbn: book.isbn,
-          qty: book.qty,
-        });
-        return newBook.save();
-      }
-    });
-
-    await Promise.all(promises); // Wait for all save operations
+    await Book.insertMany(bookData)
 
     mongoose.connection.close(); // Close connection after completion
     console.log("Books updated");
--- database/schema/bookSchema.js
+++ database/schema/bookSchema.js
@@ -1,19 +1,41 @@
 const { Schema, model } = require("mongoose");
 
+const industryIdentifierSchema = new Schema({
+  type: String,
+  identifier: String
+});
+
+const imageLinks = new Schema({
+  smallThumbnail: String,
+  thumbnail: String
+});
+
 const bookSchema = new Schema({
   title: { type: String, immutable: true },
-  author: String,
-  year: Number,
-  pages: Number,
-  genres: [String],
-  language: String,
-  isFiction: Boolean,
+  subtitle: String,
+  authors: [String],
   publisher: String,
-  bookPrice: Number,
-  isbn: Number,
-  qty: Number,
+  publishedDate: String,
+  description: String,
+  industryIdentifiers: [ industryIdentifierSchema ],
+  pageCount: Number,
+  printType: String,
+  categories: [String],
+  maturityRating: String,
+  allowAnonLogging: Boolean,
+  contentVersion: String,
+  imageLinks: imageLinks,
+  language: String,
+  previewLink: String,
+  infoLink: String,
+  canonicalVolumeLink: String,
+  price: Number,
+  quantity: Number,
   rating: { type: Number, default: 0 },
-});
+  isbn: { type: String,}
+})
+
+
 
 const Book = model("Book", bookSchema);
 
