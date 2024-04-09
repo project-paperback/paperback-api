a3647cfe6c78e4b327702a35097629542fde1183
Riccardo Foti <riccardofoti97@gmail.com>
2024-04-09 11:24:19 +0100

    Fix updateBookRating, the ratingAverage is now set to 0 if no ratings have been left

--- server/utilities/utils.js
+++ server/utilities/utils.js
@@ -2,20 +2,24 @@ const mongoose = require("mongoose");
 const { Book, Review, User } = require("../../database/schema/schemaIndex");
 
 async function updateBookRating(book_id) {
-  // Get all the reviews
-  const allReviews = await Review.find({ bookId: book_id });
-
-  let ratingsSum = 0;
-  const aggregate = allReviews.forEach(
-    (review) => (ratingsSum += review.rating)
-  );
-
-  const ratingAverage = ratingsSum / allReviews.length;
-  const averageResult = Number(ratingAverage.toFixed(1));
-  // get the book by id
-  await Book.findByIdAndUpdate(book_id, {
-    $set: { rating: averageResult, timesRated: allReviews.length },
-  });
+  try {
+    // Get all the reviews
+    const allReviews = await Review.find({ bookId: book_id });
+  
+    let ratingsSum = 0;
+    const aggregate = allReviews.forEach(
+      (review) => (ratingsSum += review.rating)
+    );
+    // if the result is a truthy assign result, if it's NaN(falsy) assign 0;
+    const ratingAverage = ratingsSum / allReviews.length ? ratingsSum / allReviews.length : 0;
+    const averageResult = Number(ratingAverage.toFixed(1));
+    // get the book by id
+    await Book.findByIdAndUpdate(book_id, {
+      $set: { rating: averageResult, timesRated: allReviews.length },
+    });
+  } catch (error) {
+    console.log(error, "from utils.js")
+  }
 }
 
 module.exports = { updateBookRating };
